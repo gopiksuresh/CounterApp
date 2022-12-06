aa8e1a7b3f4180d6bd7200d9d4ffe9163c80e086
gopiksuresh <gopiksuresh@gmail.com>
2022-12-06 11:09:00 +0530

    CounterApp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[counter,setCounter]= useState(0);
+  function incrementCounter(){
+    setCounter(counter+1)
+  }
+  function decrementCounter(){
+    setCounter(counter-1)
+  }
+  function resetCounter(){
+    setCounter(0)
+  }
   return (
-    <div className="App">
+    <div style={{
+      width: '500px',
+      background: 'yellow',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      border: '1px solid black',
+      padding: '50px',
+      marginTop: '100px',
+      marginLeft:'500px'
+    }} 
+    className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Counter Application</h4>
+        <h1>{counter}</h1>
+        <button 
+        style={{
+          background: 'green',
+          color: 'white',
+          border: '1px solid green',
+          padding: '10px',
+          margin: '10px'
+        }}
+        onClick={incrementCounter}>Increment</button>
+        <button
+        style={{
+          background: 'red',
+          color: 'white',
+          border: '1px solid red',
+          padding: '10px',
+          margin: '10px'
+        }}
+        onClick={decrementCounter}>Decrement</button>
+        <button
+        style={{
+          background: 'blue',
+          color: 'white',
+          border: '1px solid blue',
+          padding: '10px',
+          margin: '10px'
+        }}
+        onClick={resetCounter}>Reset</button>
       </header>
     </div>
   );
